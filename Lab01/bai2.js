2331f8e20cb1b2f6455995fc8ed09295bbbf6fee
Trần Nguyễn Ngọc Hùng <trannguyenngochung.2005@gmail.com>
2026-03-23 22:59:32 +0700

    Lab02

---
+++ Lab01/bai2.js
@@ -0,0 +1,27 @@
+{
+  "type": "Compass Connections",
+  "version": {
+    "$numberInt": "1"
+  },
+  "connections": [
+    {
+      "id": "d991ea9a-069e-400a-a8f5-1262bfa9927a",
+      "lastUsed": {
+        "$date": {
+          "$numberLong": "1772519225233"
+        }
+      },
+      "favorite": {
+        "name": "23520578",
+        "color": "color1"
+      },
+      "savedConnectionType": "favorite",
+      "connectionOptions": {
+        "connectionString": "mongodb+srv://23520578:22052005@cluster0.lemivnm.mongodb.net/",
+        "oidc": {
+          "serializedState": "eyJvaWRjUGx1Z2luU3RhdGVWZXJzaW9uIjoxLCJzdGF0ZSI6W119"
+        }
+      }
+    }
+  ]
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+Sinh viên: Trần Nguyễn Ngọc Hùng  
+MSSV: 23520578  
+Lớp: IE213.Q21.1 
+Môn học: IE213.Q21
+## Danh sách bài thực hành
+- TH1: MongoDB
+- TH2: Express
+- TH3: REST API
+
+Thông tin sinh viên: họ tên, MSSV, lớp
+• Môn học: IE213.Q21
+• Danh sách các lab
+• Mô tả ngắn gọn từng lab
+• Cách chạy chương trình
+• Kết quả thực hiện
+• Hình ảnh minh họa / output / screenshot nếu cần
+• Những nội dung đã hoàn thành và chưa hoàn thành
